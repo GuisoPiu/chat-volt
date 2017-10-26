9ef9a5356b87469b2af050debfd2c1f5344d5e50
GuisoPiu <gruposiice.hr@gmail.com>
2017-10-26 20:28:58 +0000

    create form to send messages

--- app/main/controllers/main_controller.rb
+++ app/main/controllers/main_controller.rb
@@ -1,14 +1,30 @@
 # By default Volt generates this controller for your Main component
 module Main
   class MainController < Volt::ModelController
+    
+    model :store    
+
     def index
       # Add code for when the index view is loaded
     end
-
-    def about
-      # Add code for when the about view is loaded
+    
+    def select_conversation(user)
+      params._user_id = user._id
+       page._new_message = ''
+    end
+    
+    def send_message
+      unless page._new_message.strip.empty?
+        _messages << { sender_id: Volt.user._id, receiver_id: params._user_id, text: page._new_message }
+        page._new_message = ''
+      end
+    end
+    
+    def current_conversation
+      _messages.find({ "$or" => [{ sender_id: Volt.user._id, receiver_id: params._user_id }, { sender_id: params._user_id, receiver_id: Volt.user._id }] })
     end
 
+ 
     private
 
     # The main template contains a #template binding that shows another
